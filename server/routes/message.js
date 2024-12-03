20291edb89b57583de67c54abc71b109f3eb8fc0
sushi2402 <137396488+sushi2402@users.noreply.github.com>
2024-12-03 10:49:45 +0530

    Delete server directory

--- server/routes/message.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { sendMessage, getMessages } from "../controllers/messageControllers.js";
-import { Auth } from "../middleware/user.js";
-router.post("/", Auth, sendMessage);
-router.get("/:chatId", Auth, getMessages);
-export default router;
